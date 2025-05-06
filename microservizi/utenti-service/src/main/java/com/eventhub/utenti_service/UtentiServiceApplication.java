15b5d22cf1e50ea614ea90d7a41da5fb8be01f02
Matteo Marengo <marengomatteo01@gmail.com>
2025-05-06 23:34:56 +0200

    Creato microservizio per utenti

---
+++ microservizi/utenti-service/src/main/java/com/eventhub/utenti_service/UtentiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eventhub.utenti_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtentiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtentiServiceApplication.class, args);
+	}
+
+}
