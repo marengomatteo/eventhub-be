be7d4869b129b05732893dc00bbf078c150da6bf
AlessioGhione <aleghoio14@gmail.com>
2025-05-28 11:37:13 +0200

    basic gateway

---
+++ gateway/src/main/java/com/eventhub/gateway/controller/GatewayController.java
@@ -0,0 +1,21 @@
+package com.eventhub.gateway.controller;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.mvc.ProxyExchange;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GatewayController {
+
+    @Value("${remote.home}")
+    private URI home;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> proxy(ProxyExchange<byte[]> proxy) throws Exception {
+        return proxy.uri(home.toString() + "/image/png").get();
+    }
+}
