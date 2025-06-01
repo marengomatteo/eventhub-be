9d07a9f1f8a8e3b09b36821570a3f83cb5e19ab5
Matteo Marengo <marengomatteo01@gmail.com>
2025-06-01 16:43:33 +0200

    da gateway mvc a spring cloud gateway

--- gateway/src/main/java/com/eventhub/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/eventhub/gateway/GatewayApplication.java
@@ -2,12 +2,9 @@ package com.eventhub.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import com.eventhub.gateway.config.RouteConfig;
 
 @SpringBootApplication
-@EnableConfigurationProperties(RouteConfig.class)
 public class GatewayApplication {
 
     public static void main(String[] args) {
