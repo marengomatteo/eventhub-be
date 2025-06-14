65d7aab95971df544f535a34770c42cc43286514
AlessioGhione <aleghoio14@gmail.com>
2025-06-14 19:06:19 +0200

    updateevnet error fix

--- microservizi/event_service/src/main/java/com/eventhub/event_service/service/EventService.java
+++ microservizi/event_service/src/main/java/com/eventhub/event_service/service/EventService.java
@@ -1,5 +1,7 @@
 package com.eventhub.event_service.service;
 
+import java.util.Optional;
+
 import org.springframework.core.io.ClassPathResource;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,18 +22,17 @@ public class EventService {
 
     public void newEvent(EventRequest request) {
         try {
-        Event e = new Event();
-        e.setEventName(request.getEventName());
-        e.setStartDate(request.getStartDate());
-        e.setEndDate(request.getEndDate());
-        e.setTime(request.getTime());
-        e.setLocation(request.getLocation());
-        e.setMaxPartecipants(request.getMaxPartecipants());
-        e.setEventType(request.getEventType());
-        e.setDescription(request.getDescription());
-        Event esaved = eventRepository.save(e);
-        log.info(esaved.getId());
-
+            Event e = new Event();
+            e.setEventName(request.getEventName());
+            e.setStartDate(request.getStartDate());
+            e.setEndDate(request.getEndDate());
+            e.setTime(request.getTime());
+            e.setLocation(request.getLocation());
+            e.setMaxPartecipants(request.getMaxPartecipants());
+            e.setEventType(request.getEventType());
+            e.setDescription(request.getDescription());
+            Event esaved = eventRepository.save(e);
+            log.info(esaved.getId());
         } catch (Exception e) { //TODO: errore troppo generico
             log.error("Error creating new event: ", e);
             throw new RuntimeException("Failed to create new event", e);
@@ -40,18 +41,24 @@ public class EventService {
 
     public void updateEvent(EventRequest request) {
         try {
-        Event e = eventRepository.findById(request.getId())
-        e.setEventName(request.getEventName());
-        e.setStartDate(request.getStartDate());
-        e.setEndDate(request.getEndDate());
-        e.setTime(request.getTime());
-        e.setLocation(request.getLocation());
-        e.setMaxPartecipants(request.getMaxPartecipants());
-        e.setEventType(request.getEventType());
-        e.setDescription(request.getDescription());
-        Event esaved = eventRepository.save(e);
-        log.info(esaved.getId());
+            Optional<Event> e = eventRepository.findById(request.getId());
+            if (e.isEmpty()) {
+                log.error("Event with id {} not found for update", request.getId());
+                throw new RuntimeException("Event not found while updating");
+            }else {
+                Event event = e.get();
+                event.setEventName(request.getEventName());
+                event.setStartDate(request.getStartDate());
+                event.setEndDate(request.getEndDate());
+                event.setTime(request.getTime());
+                event.setLocation(request.getLocation());
+                event.setMaxPartecipants(request.getMaxPartecipants());
+                event.setEventType(request.getEventType());
+                event.setDescription(request.getDescription());
 
+                Event esaved = eventRepository.save(event);
+                log.info(esaved.getId());
+            }
         } catch (Exception e) {
             log.error("Error updating event {}: ", request.getId(), e);
             throw new RuntimeException("Failed to update event", e);
