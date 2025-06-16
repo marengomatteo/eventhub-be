c6184f8bd135f898f09c299bde92a6824d2a20d0
AlessioGhione <aleghoio14@gmail.com>
2025-06-16 18:31:43 +0200

    agenda_service, roba base a csao incomplete e non funzionanti

---
+++ microservizi/agenda_service/src/main/java/com/eventhub/agenda_service/controller/AgendaController.java
@@ -0,0 +1,49 @@
+package com.eventhub.agenda_service.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eventhub.agenda_service.dto.AgendaResponse;
+import com.eventhub.agenda_service.service.AgendaService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+@RestController 
+@RequestMapping("/agenda")
+@RequiredArgsConstructor
+@Slf4j
+public class AgendaController {
+
+    private final AgendaService agendaService;
+
+    @GetMapping("")
+    public ResponseEntity<AgendaResponse> getAgendaByEvent() {
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("")
+    public void createAgenda(@RequestBody Object agendaRequest) {
+        return;
+    }
+
+    @PutMapping("/{id}")
+    public void updateAgenda(@PathVariable("id") String id) {
+        agendaService.updateAgenda(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAgenda(@PathVariable("id") String id) {
+        agendaService.deleteAgenda(id);
+    }
+    
+}
