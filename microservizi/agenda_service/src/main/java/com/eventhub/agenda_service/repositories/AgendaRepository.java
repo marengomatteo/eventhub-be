861497210401dab7c51d44e2f61d93121c68b056
AlessioGhione <aleghoio14@gmail.com>
2025-06-17 12:31:05 +0200

    creati agendaRequest, mapper, repository e application-docker.properties

---
+++ microservizi/agenda_service/src/main/java/com/eventhub/agenda_service/repositories/AgendaRepository.java
@@ -0,0 +1,7 @@
+package com.eventhub.agenda_service.repositories;
+
+import com.eventhub.agenda_service.entities.Agenda;
+
+public interface AgendaRepository extends JpaRepository<Agenda, Long>{
+
+}
