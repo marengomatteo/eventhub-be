af716a9906aeb6e4639f79ca18f0ff066fece250
AlessioGhione <aleghoio14@gmail.com>
2025-06-17 12:31:41 +0200

    update to pom e iniziato a scrivere delle brutte funzioni

--- microservizi/agenda_service/src/main/java/com/eventhub/agenda_service/dto/AgendaResponse.java
+++ microservizi/agenda_service/src/main/java/com/eventhub/agenda_service/dto/AgendaResponse.java
@@ -1,16 +1,22 @@
 package com.eventhub.agenda_service.dto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.eventhub.agenda_service.entities.Session;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AgendaResponse {
 
     private String id;
     private String eventName;
     private List<Session> sessionsList; 
-
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
