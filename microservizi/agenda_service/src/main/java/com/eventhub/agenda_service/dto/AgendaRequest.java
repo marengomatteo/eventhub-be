4cdaac961b76c45aa63f842087b644b2eba30160
AlessioGhione <aleghoio14@gmail.com>
2025-06-17 12:31:05 +0200

    creati agendaRequest, mapper, repository e application-docker.properties

---
+++ microservizi/agenda_service/src/main/java/com/eventhub/agenda_service/dto/AgendaRequest.java
@@ -0,0 +1,14 @@
+package com.eventhub.agenda_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AgendaRequest {
+
+    private String eventName;
+
+}
