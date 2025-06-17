5fd45aba9a9acc69b411fb3d45b1b8811af4144c
AlessioGhione <aleghoio14@gmail.com>
2025-06-17 12:31:41 +0200

    update to pom e iniziato a scrivere delle brutte funzioni

--- microservizi/agenda_service/src/main/java/com/eventhub/agenda_service/controller/AgendaController.java
+++ microservizi/agenda_service/src/main/java/com/eventhub/agenda_service/controller/AgendaController.java
@@ -1,11 +1,8 @@
 package com.eventhub.agenda_service.controller;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -27,12 +24,13 @@ public class AgendaController {
     private final AgendaService agendaService;
 
     @GetMapping("")
-    public ResponseEntity<AgendaResponse> getAgendaByEvent() {
+    public ResponseEntity<AgendaResponse> getAgendaByEvent(@PathVariable("id") String id) {
+        AgendaResponse agenda = agendaService.getAgendaByEvent(id);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("")
-    public void createAgenda(@RequestBody Object agendaRequest) {
+    public void newAgenda(@RequestBody Object agendaRequest) {
         return;
     }
 
--- microservizi/agenda_service/src/main/java/com/eventhub/agenda_service/service/AgendaService.java
+++ microservizi/agenda_service/src/main/java/com/eventhub/agenda_service/service/AgendaService.java
@@ -1,11 +1,15 @@
 package com.eventhub.agenda_service.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
-
+import com.eventhub.agenda_service.dto.AgendaRequest;
 import com.eventhub.agenda_service.dto.AgendaResponse;
+import com.eventhub.agenda_service.entities.Agenda;
+import com.eventhub.agenda_service.repositories.AgendaRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,13 +19,34 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AgendaService {
     
+    private final AgendaRepository agendaRepository;
+    private final AgendaMapper agendaMapper;
 
-    public AgendaResponse getAgendaByEvent() {
-      return new AgendaResponse();
+
+
+    public AgendaResponse getAgendaByEvent(String id) {
+        try{
+            AgendaResponse response = agendaRepository.findById(id).orElseThrow(() -> {
+                log.error("Agenda with event id {} not found", id);
+                throw new RuntimeException("Agenda not found");
+            });     
+        } catch (Exception e) {
+            log.error("Error retrieving agenda for event with id {}: {}", id, e.getMessage());
+            throw new RuntimeException("Failed to retrieve agenda for event", e);
+        }
     }
 
-    public void createAgenda() {
+    public void newAgenda(AgendaRequest request) {
+        try {
+            Agenda e = agendaMapper.parse(request);
+            e.setPartecipantsList(new ArrayList<>());
+            Event esaved = eventRepository.save(e);
 
+            return esaved.getId();
+        } catch (DataAccessException e) {
+            log.error("Error creating new event: ", e);
+            throw new RuntimeException("Failed to create new event", e);
+        }
     }
 
     public void updateAgenda(String id) {
