d565935af9b49bbfda9d7d3d6aa0bb0e09e37564
AlessioGhione <aleghoio14@gmail.com>
2025-06-19 18:13:46 +0200

    client service e .properties modificato

--- microservizi/event_service/src/main/java/com/eventhub/event_service/service/GrpcClientService.java
+++ microservizi/event_service/src/main/java/com/eventhub/event_service/service/GrpcClientService.java
@@ -1,17 +1,19 @@
-// package com.eventhub.event_service.service;
+package com.eventhub.event_service.service;
 
-// import net.devh.boot.grpc.client.inject.GrpcClient;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 
-// import com.eventhub.event_service.proto.GreeterGrpc;
+import com.eventhub.event_service.proto.GreeterGrpc;
+import com.eventhub.event_service.proto.HelloRequest;
+import com.eventhub.event_service.proto.HelloResponse;
 
-// public class GrpcClientService {
+public class GrpcClientService {
     
-//     @GrpcClient("local-grpc-server")
-//     private GreeterGrpc.GreeterBlockingStub greeterStub;
+    @GrpcClient("local-grpc-server")
+    private GreeterGrpc.GreeterBlockingStub greeterStub;
     
-//     public String chiamaServer(String name) {
-//         HelloRequest request = HelloRequest.newBuilder().setName(name).build();
-//         HelloReply reply = greeterStub.sayHello(request);
-//         return reply.getMessage();
-//     }
-// }
+    public String chiamaServer(String name) {
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+        HelloResponse reply = greeterStub.sayHello(request);
+        return reply.getMessage();
+    }
+}
