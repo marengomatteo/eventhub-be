8c499b06b0f83eaa1152f0991a85eddaa68e4a86
Matteo Marengo <marengomatteo01@gmail.com>
2025-06-29 15:55:04 +0200

    fix repo

--- microservizi/agenda_service/src/main/java/com/eventhub/agenda_service/AgendaServiceApplication.java
+++ microservizi/agenda_service/src/main/java/com/eventhub/agenda_service/AgendaServiceApplication.java
@@ -2,10 +2,10 @@ package com.eventhub.agenda_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EntityScan({ "com.eventhub.agenda_service.entities" })
+@EnableMongoRepositories
 public class AgendaServiceApplication {
 
 	public static void main(String[] args) {
